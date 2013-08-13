7b4758d98d17024171b4b410be0d6071f9b7c046
Matthew Walster <matthew@walster.org>
2013-08-13 23:04:18 +0100

    Imported script
    'twas a frenzied 20 minutes of coding, entirely untested.
    
    It will be refined over the next few days and weeks, and pull requests
    are very welcome if anyone has any interesting functionality to add!

---
+++ rfcgrep.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+store="~/.rfcgrep"
+rsyncopts="-v -z --progress --stats"
+
+update()
+{
+	rsync -a --delete ${rsyncopts} rsync://rfc-editor.org/rfcs-text-only ${store}/rfc
+	rsync -a --delete ${rsyncopts} rsync://rfc-editor.org/ids-text-only ${store}/i-d
+}
+
+search()
+{
+	grep "$*" ${store}/${type} | ${PAGER}
+}
+
+mkdir -p ${store}/rfc/
+mkdir -p ${store}/i-d/
+
+case "$1" in
+	help|--help|-h)
+		usage
+		return 0
+		;;
+	type|--type|-t)
+		type="$2"
+		shift 2
+		;;
+	update|--update|-u)
+		update
+		return 0
+		;;
+	*)
+		search $*
+		;;
+esac
+
